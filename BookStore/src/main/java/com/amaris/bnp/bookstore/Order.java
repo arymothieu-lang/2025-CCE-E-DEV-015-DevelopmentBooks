a0d9a9502a882ed1c1eceb1c4270c9a8241443f4
amoth <ary.mothieu@gmail.com>
2025-10-16 15:28:38 +0200

    clean

--- BookStore/src/main/java/com/amaris/bnp/bookstore/Order.java
+++ BookStore/src/main/java/com/amaris/bnp/bookstore/Order.java
@@ -24,43 +24,6 @@ public class Order {
           this.books=List.of(books);
      }
 
-     public List<Order> splitOrderToDiscountSet() {
-          final List<Order> finalOrder=new ArrayList<>();
-          final Collection<Book> booksCopy = new ArrayList<>(books);
-          while (!booksCopy.isEmpty()) {
-               var bookSet = new HashSet<>(booksCopy);
-               bookSet.forEach(booksCopy::remove);
-               finalOrder.add(new Order(bookSet));
-          }
-          return finalOrder;
-     }
-
-     public double applyDiscount() {
-          double price =this.getBooks().stream()
-                  .map(Book::getPrice)
-                  .reduce(0d, Double::sum);
-          if (this.containHog()){
-               return price-(price*0.25);
-          }
-          if (this.containsNDifferentBook(4)){
-               return price-(price*.2);
-          }
-          if (this.containsNDifferentBook(3)){
-               return price-(price*.1);
-          }
-          if (this.containsNDifferentBook(2))
-               return price-(price*0.05);
-          return price;
-     }
-
-     public boolean containsNDifferentBook(int i) {
-          return new HashSet<>(books).size() >= i;
-     }
-
-     public boolean containHog() {
-          return books.containsAll(BookEnum.getBooks());
-     }
-
      public int getAvailableBookSize() {
           return (int) books.stream().distinct().count();
      }
