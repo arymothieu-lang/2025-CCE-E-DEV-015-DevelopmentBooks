ce64912458b4e3e732ce6136a815e390adce8a00
amoth <ary.mothieu@gmail.com>
2025-10-16 13:19:07 +0200

    each book price 50

--- BookStore/src/main/java/com/amaris/bnp/bookstore/Order.java
+++ BookStore/src/main/java/com/amaris/bnp/bookstore/Order.java
@@ -1,4 +1,13 @@
 package com.amaris.bnp.bookstore;
 
+import lombok.Getter;
+
+import java.util.List;
+@Getter
 public class Order {
+     private List<Book> books;
+
+     public Order(Book book) {
+          books=List.of(book);
+     }
 }
--- BookStore/src/test/java/com/amaris/bnp/bookstore/service/OrderServiceTest.java
+++ BookStore/src/test/java/com/amaris/bnp/bookstore/service/OrderServiceTest.java
@@ -1,7 +1,10 @@
 package com.amaris.bnp.bookstore.service;
 
+import com.amaris.bnp.bookstore.BookEnum;
 import com.amaris.bnp.bookstore.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -17,6 +20,15 @@ public class OrderServiceTest {
     public void shouldReturn0forAnEmptyOrder(){
         var price = orderService.getPrice(new Order());
         assertEquals(0,price);
+
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = BookEnum.class)
+    public void shouldReturn50for1Book(BookEnum book){
+        final Order order=new Order(book.getBook());
+        final var result=orderService.getPrice(order);
+        assertEquals(50f,result);
 
     }
 }
