bb8b00c865b88fadf61890cebfb7eb8de9ea217c
amoth <ary.mothieu@gmail.com>
2025-10-16 15:13:16 +0200

    fix example tests

--- BookStore/src/main/java/com/amaris/bnp/bookstore/service/OrderService.java
+++ BookStore/src/main/java/com/amaris/bnp/bookstore/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.amaris.bnp.bookstore.service;
 
+import com.amaris.bnp.bookstore.MatrixUtils;
 import com.amaris.bnp.bookstore.Order;
 import org.springframework.stereotype.Service;
 
@@ -7,12 +8,7 @@ import org.springframework.stereotype.Service;
 public class OrderService {
 
     public double getPrice(Order order){
-        Double acc = 0d;
-        for (Order order1 : order.splitOrderToDiscountSet()) {
-            Double applyDiscount = order1.applyDiscount();
-            acc = acc + applyDiscount;
-        }
-        return acc;
+        return MatrixUtils.getMinPrice(order)*50;
     }
 
 
