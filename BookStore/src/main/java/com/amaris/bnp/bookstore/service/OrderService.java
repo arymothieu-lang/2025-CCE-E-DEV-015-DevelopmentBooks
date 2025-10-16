66d9eb3970bccf93cabc7481ce17313a9eb08144
amoth <ary.mothieu@gmail.com>
2025-10-16 13:12:50 +0200

    create getPrice

--- BookStore/src/main/java/com/amaris/bnp/bookstore/service/OrderService.java
+++ BookStore/src/main/java/com/amaris/bnp/bookstore/service/OrderService.java
@@ -1,7 +1,11 @@
 package com.amaris.bnp.bookstore.service;
 
+import com.amaris.bnp.bookstore.Order;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
+    public Object getPrice(Order order) {
+        return null;
+    }
 }
