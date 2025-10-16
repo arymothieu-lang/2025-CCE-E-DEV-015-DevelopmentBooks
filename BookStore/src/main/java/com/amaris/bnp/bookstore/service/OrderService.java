5ab1a897631f87debdf139ea0e007fff09ee3771
amoth <ary.mothieu@gmail.com>
2025-10-16 13:08:15 +0200

    create class tests

--- BookStore/src/main/java/com/amaris/bnp/bookstore/service/OrderService.java
+++ BookStore/src/main/java/com/amaris/bnp/bookstore/service/OrderService.java
@@ -1,4 +1,7 @@
 package com.amaris.bnp.bookstore.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
 }
--- BookStore/src/test/java/com/amaris/bnp/bookstore/service/OrderServiceTest.java
+++ BookStore/src/test/java/com/amaris/bnp/bookstore/service/OrderServiceTest.java
@@ -1,4 +1,22 @@
 package com.amaris.bnp.bookstore.service;
 
+import com.amaris.bnp.bookstore.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(classes = OrderService.class)
 public class OrderServiceTest {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Test
+    public void shouldReturn0forAnEmptyOrder(){
+        var price = orderService.getPrice(new Order());
+        assertEquals(0,price);
+
+    }
 }
